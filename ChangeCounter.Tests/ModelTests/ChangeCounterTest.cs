90e61637b7e32296f2b72a9800d7770a1616400f
David Tumpowsky <tumpowskydavid@gmail.com>
2018-04-24 16:36:36 -0700

    stores leftover change as price for quarters, dimes, nickels, and pennies

--- ChangeCounter.Tests/ModelTests/ChangeCounterTest.cs
+++ ChangeCounter.Tests/ModelTests/ChangeCounterTest.cs
@@ -14,7 +14,15 @@ namespace CoinApp.Tests
         {
             Change testChangeCounter = new Change(0.95m);
 
-            Assert.AreEqual(0.20m, testChangeCounter.CalculateChange(0.95m));
+            Assert.AreEqual(0.20m, testChangeCounter.ChangeAfterQuarters(0.95m));
+        }
+
+        [TestMethod]
+        public void CountChange_ReturnDimeChange_Decimal()
+        {
+            Change testChangeCounter = new Change(0.0m, 0.15m);
+
+            Assert.AreEqual(0.05m, testChangeCounter.ChangeAfterDimes(0.15m));
         }
     }
 }
--- ChangeCounter/Models/ChangeCounter.cs
+++ ChangeCounter/Models/ChangeCounter.cs
@@ -8,43 +8,50 @@ namespace CoinApp.Models
     {
         // private string _userInput;
         private decimal _quarters;
-        // private decimal _dimes;
+        private decimal _dimes;
         // private decimal _nickels;
         // private decimal _pennies;
         // private string _changeOutput;
         private static List<Change> _instances = new List<Change> {};
 
-        public Change(decimal quarters)
+        public Change(decimal quarters = 0.0m, decimal dimes = 0.0m, decimal nickels = 0.0m, decimal pennies = 0.0m)
         {
             // _userInput = userInput;
             _quarters = quarters;
-            // _dimes = dimes;
+            _dimes = dimes;
             // _nickels = nickels;
             // _pennies = pennies;
             // _changeOutput = changeOutput;
             // _instances.Add(this);
         }
 
-        // public string GetUserInput()
-        // {
-        //     return _userInput;
-        // }
-
-        // public void SetUserInput(newUserInput)
-        // {
-        //     _userInput = newUserInput;
-        // }
-
-        public decimal CalculateChange(decimal price)
+        public decimal ChangeAfterQuarters(decimal price)
         {
             _quarters = 0.25m;
             price %= _quarters;
             return price;
-            // _dimes = price / 0.10m;
-            // _nickels = price / 0.05m;
-            // _pennies = price / 0.01m;
 
         }
 
+        public decimal ChangeAfterDimes(decimal price)
+        {
+            _dimes = 0.10m;
+            price %= _dimes;
+            return price;
+        }
+
+        public decimal ChangeAfterNickels(decimal price)
+        {
+            _nickels = 0.10m;
+            price %= _nickels;
+            return price;
+        }
+
+        public decimal ChangeAfterPennies(decimal price)
+        {
+            _pennies = 0.10m;
+            price %= _pennies;
+            return price;
+        }
     }
 }
